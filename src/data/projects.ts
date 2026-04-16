b8f3d97cedd95eb70e241ace2289a9f60cc317a9
Alessio Cascini <cascini.alessio@studenti-ittfedifermi.edu.it>
2026-04-16 10:31:00 +0200

    feat: logo

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,9 +1,9 @@
-import PhoenixIcon from '../assets/logos/Phoenix-logo.svg?raw';
-import N8nIcon from '../assets/logos/N8n-logo.svg?raw';
-import SupabaseIcon from '../assets/logos/Supabase-logo.svg?raw';
-import PosthogIcon from '../assets/logos/posthog-logo.svg?raw';
-import PythonIcon from '../assets/logos/Python-logo.svg?raw';
-import TailwindIcon from '../assets/logos/Tailwind-logo.svg?raw';
+import PhoenixIcon from '../assets/logos/Astro-logo.svg?raw';
+import N8nIcon from '../assets/logos/Astro-logo.svg?raw';
+import SupabaseIcon from '../assets/logos/Astro-logo.svg?raw';
+import PosthogIcon from '../assets/logos/Astro-logo.svg?raw';
+import PythonIcon from '../assets/logos/Astro-logo.svg?raw';
+import TailwindIcon from '../assets/logos/Astro-logo.svg?raw';
 
 
 export const projects = [
